b4e63650608e4651c176a0d15c0bd77e595abb53
zzergAtStage <hightemplar98@iclod.com>
2024-02-23 14:40:58 +0100

    dev: Some features for public-data.html

--- src/main/java/com/zergatstage/s07secweb/controller/MainController.java
+++ src/main/java/com/zergatstage/s07secweb/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.zergatstage.s07secweb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,5 +11,11 @@ public class MainController {
     @GetMapping("/hello")
     public String mainPage(){
         return "hello";
+    }
+
+    @GetMapping("/public-data")
+    public String getPublicData(Model model){
+        model.addAttribute("sampleMessage", "Just text about pega for unauthenticated users");
+        return "public-data";
     }
 }
--- src/main/java/com/zergatstage/s07secweb/securingweb/WebSecurityConfig.java
+++ src/main/java/com/zergatstage/s07secweb/securingweb/WebSecurityConfig.java
@@ -19,7 +19,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/home").permitAll()
+                        .requestMatchers("/", "/home", "/public-data").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
